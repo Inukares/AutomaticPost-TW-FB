c585235c6ee600921baa97b63cdfc62f34193e87
Piotr <zelowski@gmail.com>
2018-03-21 17:16:02 +0100

    cleaned code, posting to twitter works

--- backend/passport.js
+++ backend/passport.js
@@ -1,21 +1,26 @@
-'use strict';
+"use strict";
 
-var passport = require('passport'),
-  TwitterTokenStrategy = require('passport-twitter-token'),
-  User = require('mongoose').model('User'),
-  twitterConfig = require('./twitter.config.js');
+var passport = require("passport"),
+  TwitterTokenStrategy = require("passport-twitter-token"),
+  User = require("mongoose").model("User"),
+  twitterConfig = require("./configs/twitterConfig");
 
-module.exports = function () {
-
-  passport.use(new TwitterTokenStrategy({
-      consumerKey: twitterConfig.consumerKey,
-      consumerSecret: twitterConfig.consumerSecret,
-      includeEmail: true
-    },
-    function (token, tokenSecret, profile, done) {
-      User.upsertTwitterUser(token, tokenSecret, profile, function(err, user) {
-        return done(err, user);
-      });
-    }));
-
+module.exports = function() {
+  passport.use(
+    new TwitterTokenStrategy(
+      {
+        consumerKey: twitterConfig.consumerKey,
+        consumerSecret: twitterConfig.consumerSecret,
+        includeEmail: true
+      },
+      function(token, tokenSecret, profile, done) {
+        User.upsertTwitterUser(token, tokenSecret, profile, function(
+          err,
+          user
+        ) {
+          return done(err, user);
+        });
+      }
+    )
+  );
 };
